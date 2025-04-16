22c65abd6ec0ea4f4f14742b86e425b8665c6436
Marouan SOUISSI <m.souissi@kiantec.co>
2025-04-16 12:01:16 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -21,84 +21,72 @@ variable "do_token" {
   sensitive   = true
 }
 
-variable "ssh_key_id" {
-  description = "DigitalOcean SSH Key ID"
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster"
   type        = string
-}
-
-
-variable "droplet_name" {
-  description = "Name of the Droplet"
-  type        = string
-  default     = "k6-vm"
+  default     = "k8s-k6-cluster"
 }
 
 variable "region" {
-  description = "Region where Droplet will be created"
+  description = "Region where the cluster will be created"
   type        = string
   default     = "nyc3"
 }
 
-variable "size" {
-  description = "Droplet size"
+variable "k8s_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.32.2-do.0"
+}
+
+variable "node_size" {
+  description = "Droplet size for worker nodes"
   type        = string
   default     = "s-2vcpu-4gb"
 }
 
-variable "image" {
-  description = "Droplet base image"
-  type        = string
-  default     = "ubuntu-22-04-x64"
+variable "node_count" {
+  description = "Number of worker nodes"
+  type        = number
+  default     = 2
 }
-  variable "ssh_private_key" {
-  description = "Content of the private SSH key"
-  type        = string
-  sensitive   = true
-}
-
 
 # ===============================
-# RESOURCE: DROPLET
+# RESOURCE: KUBERNETES CLUSTER
 # ===============================
 
-resource "digitalocean_droplet" "k6_vm" {
-  name     = var.droplet_name
-  region   = var.region
-  size     = var.size
-  image    = var.image
-  ssh_keys = [var.ssh_key_id]
+resource "digitalocean_kubernetes_cluster" "k8s_cluster" {
+  name    = var.cluster_name
+  region  = var.region
+  version = var.k8s_version
 
-  connection {
-    type        = "ssh"
-    user        = "root"
-    host        = self.ipv4_address
-    private_key = var.ssh_private_key
-    timeout     = "3m"
-  }
-
-
-
-
-  provisioner "file" {
-    source      = "script.sh"
-    destination = "/tmp/script.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sleep 120",
-      "while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 || sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1; do echo 'Waiting for apt...'; sleep 5; done",
-      "chmod +x /tmp/script.sh",
-      "/tmp/script.sh"
-    ]
+  node_pool {
+    name       = "worker-pool"
+    size       = var.node_size
+    node_count = var.node_count
+    auto_scale = false
   }
 }
+resource "null_resource" "install_k6_operator" {
+  depends_on = [digitalocean_kubernetes_cluster.k8s_cluster]
+
+  provisioner "local-exec" {
+    command = "bash ${path.module}/bootstrap-k6.sh"
+  }
+}
+
 
 # ===============================
 # OUTPUT
 # ===============================
 
-output "droplet_ip" {
-  description = "Public IP of the created droplet"
-  value       = digitalocean_droplet.k6_vm.ipv4_address
+output "cluster_id" {
+  description = "ID of the created Kubernetes cluster"
+  value       = digitalocean_kubernetes_cluster.k8s_cluster.id
+}
+
+output "kubeconfig" {
+  description = "Kubeconfig for accessing the Kubernetes cluster"
+  value       = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].raw_config
+  sensitive   = true
 }
