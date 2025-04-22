77bfe9bfdd0d739fd03d6c610c4b85b065e6624e
Marouan SOUISSI <m.souissi@kiantec.co>
2025-04-22 09:04:23 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -15,32 +15,51 @@ terraform {
   }
 }
 
-# ✅ Declare required variable
-variable "do_token" {
-  description = "DigitalOcean API token"
-  type        = string
-  sensitive   = true
-}
-
 provider "digitalocean" {
   token = var.do_token
 }
 
+variable "do_token" {
+  description = "DigitalOcean API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "cluster_name" {
+  default     = "k8s-k6-cluster"
+  description = "Kubernetes cluster name"
+}
+
+variable "region" {
+  default     = "nyc3"
+  description = "DigitalOcean region"
+}
+
+variable "k8s_version" {
+  default     = "1.32.2-do.0"
+  description = "Kubernetes version"
+}
+
+variable "node_size" {
+  default     = "s-2vcpu-4gb"
+  description = "Node size"
+}
+
+variable "node_count" {
+  default     = 2
+  description = "Number of nodes"
+}
+
 resource "digitalocean_kubernetes_cluster" "k8s_cluster" {
-  name    = "k8s-k6-cluster"
-  region  = "nyc3"
-  version = "1.32.2-do.0"
+  name    = var.cluster_name
+  region  = var.region
+  version = var.k8s_version
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-2vcpu-4gb"
-    node_count = 2
+    size       = var.node_size
+    node_count = var.node_count
   }
-}
-
-resource "local_file" "kubeconfig_yaml" {
-  content  = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].raw_config
-  filename = "${path.module}/kubeconfig_do.yaml"
 }
 
 provider "kubernetes" {
@@ -57,23 +76,6 @@ provider "helm" {
   }
 }
 
-resource "time_sleep" "wait_for_cluster" {
-  depends_on = [digitalocean_kubernetes_cluster.k8s_cluster]
-  create_duration = "90s"
-}
-
-resource "kubernetes_config_map" "cluster_ready" {
-  metadata {
-    name = "cluster-ready-check"
-  }
-
-  data = {
-    ready = "true"
-  }
-
-  depends_on = [time_sleep.wait_for_cluster]
-}
-
 resource "helm_release" "k6_operator" {
   name             = "k6-operator"
   repository       = "https://grafana.github.io/helm-charts"
@@ -82,12 +84,19 @@ resource "helm_release" "k6_operator" {
   create_namespace = true
   wait             = true
   timeout          = 300
+}
 
-  depends_on = [
-    kubernetes_config_map.cluster_ready
-  ]
+
+resource "local_file" "kubeconfig_yaml" {
+  content  = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].raw_config
+  filename = "${path.module}/kubeconfig_do.yaml"
+}
+
+output "kubeconfig_raw" {
+  value     = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].raw_config
+  sensitive = true
 }
 
 output "kubeconfig_path" {
   value = abspath(local_file.kubeconfig_yaml.filename)
-}
+}  
