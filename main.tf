ccb487908ee3eb4fcb75a71e343b4d4c37654de4
marwensouissi <marwen.souissi00@gmail.com>
2025-07-14 10:48:41 +0100

    tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ variable "region" {
 }
 
 variable "k8s_version" {
-  default     = "1.32.2-do.0"
+  default     = "1.33.1-do.1"
   description = "Kubernetes version"
 }
 
@@ -64,17 +64,14 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+# Use kubeconfig file instead of inline creds
 provider "kubernetes" {
-  host                   = digitalocean_kubernetes_cluster.k8s_cluster.endpoint
-  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].cluster_ca_certificate)
-  token                  = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].token
+  config_path = "${path.module}/kubeconfig_do.yaml"
 }
 
 provider "helm" {
   kubernetes {
-    host                   = digitalocean_kubernetes_cluster.k8s_cluster.endpoint
-    cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].cluster_ca_certificate)
-    token                  = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].token
+    config_path = "${path.module}/kubeconfig_do.yaml"
   }
 }
 
@@ -103,13 +100,66 @@ resource "helm_release" "k6_operator" {
   create_namespace = true
   wait             = true
   timeout          = 300
+
+  depends_on = [
+    digitalocean_kubernetes_cluster.k8s_cluster,
+    local_file.kubeconfig_yaml
+  ]
+   provisioner "local-exec" {
+    command = <<EOT
+      echo "Waiting for Kubernetes API to be ready..."
+      for i in {1..30}; do
+        if KUBECONFIG=${path.module}/kubeconfig_do.yaml kubectl get nodes; then
+          echo "Kubernetes API is ready!"
+          exit 0
+        else
+          echo "Retry $i/10: Kubernetes not ready yet..."
+          sleep 10
+        fi
+      done
+      echo "Timed out waiting for Kubernetes API."
+      exit 1
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+
 }
+
+
+
 
 resource "local_file" "kubeconfig_yaml" {
   count    = var.enable_k6_operator ? 1 : 0
   content  = digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].raw_config
   filename = "${path.module}/kubeconfig_do.yaml"
 }
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  namespace        = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "5.51.6" # You can use latest or pin a version
+  create_namespace = true
+  wait             = true
+  timeout          = 300
+
+  values = [
+    <<EOF
+server:
+  service:
+    type: LoadBalancer
+EOF
+  ]
+
+  depends_on = [
+    digitalocean_kubernetes_cluster.k8s_cluster,
+    local_file.kubeconfig_yaml
+  ]
+}
+
+
+
 
 ##########################
 # OUTPUTS
@@ -124,3 +174,13 @@ output "kubeconfig_path" {
   value       = var.enable_k6_operator ? abspath(local_file.kubeconfig_yaml[0].filename) : null
   description = "Path to the generated kubeconfig"
 }
+
+output "argocd_server_url" {
+  value       = "http://${helm_release.argocd.name}.${helm_release.argocd.namespace}.svc.cluster.local"
+  description = "Internal service URL for ArgoCD"
+}
+
+output "argocd_namespace" {
+  value       = "argocd"
+  description = "Namespace where ArgoCD is deployed"
+} 
